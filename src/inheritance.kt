f72b93cf6a33b522e0974c369bc49ad4907a3cbb
mercy021 <kinyanjuimercywangari@gmail.com>
2021-03-31 16:49:13 +0300

    first commit

--- src/inheritance.kt
+++ src/inheritance.kt
@@ -1,3 +1,7 @@
+
+
+
+
 fun main() {
     var gari=Car("buggatti","legacy","black",40)
     gari.carry(100)
@@ -8,23 +12,27 @@ fun main() {
     println(basi.max(4688.76.toDouble()*basi.capacity))
 
 
-}
-class Car(var make:String,var model:String,var color:String,var capacity:Int){
-    fun carry(people:Int) {
-        println("carry $people passengers")
-        var people = people - capacity
-        if (people <= capacity) {
+
+
+    }
+    open class Mat( var make:String,var model:String,var color:String,var capacity:Int) {
+        fun carry(people:Int) {
             println("carry $people passengers")
-        } else {
-            println("over capacity by $people")
+            var people = people - capacity
+            if (people <= capacity) {
+                println("carry $people passengers")
+            } else {
+                println("over capacity by $people")
+            }
         }
-    }
-        fun calculateParkingFees(hours:Int):Int{
-        return hours * 20
+        fun calculateParkingFees(hours:Int):Int {
+            return hours * 20
+        }
+        }
+class Car( make:String, model:String, color:String, capacity:Int):Mat(make,model,color,capacity){
 
-    }
 
-    }
+}
 class Bus(var make:String,var model:String,var color: String,var capacity:Int) {
     fun max(fare:Double):Double{
         var x=0
@@ -32,6 +40,9 @@ class Bus(var make:String,var model:String,var color: String,var capacity:Int) {
 
     }
 }
+
+
+
 
 
 
